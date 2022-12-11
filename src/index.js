9432a6616207bb8fd2e66b556d826e69c4f70e4f
Alex Burcea <alexandruburcea1996@gmail.com>
2022-12-12 00:47:11 +0200

    #

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <HashRouter hashType="hashbang">
     {/* <React.StrictMode> */}
     <Provider store={store}>
       <App />
     </Provider>
     {/* </React.StrictMode> */}
-  </Router>
+  </HashRouter>
 );
 
 // reportWebVitals();
